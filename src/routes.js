4b43e8a8646ca370efbefb27e943942be80127c5
Ana Carla Gomes da Silva <aninhacarla-ana@outlook.com>
2020-12-08 11:20:28 -0300

    paginas: Sobre, contatos e serviços criadas, rotas adicionadas

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import HomePage from  './pages/HomePage';
+import Contatos from './pages/Contatos';
+import Sobre from './pages/Sobre';
+import Serviços from './pages/Serviços';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={HomePage}/>
+                <Route path='/Contatos'  component={Contatos}/>
+                <Route path='/Sobre'  component={Sobre}/>
+                <Route path='/Serviços' component={Serviços}/>
             </Switch>
         </BrowserRouter>
     )
