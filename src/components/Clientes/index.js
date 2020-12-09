04ff775edfa33b109912da0734f5a00ba69e3cc4
Samuel Santos <samuelssan28@gmail.com>
2020-12-09 19:35:10 -0300

    Adicionando o componente Clientes

---
+++ src/components/Clientes/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import "./styles.css";
+
+//Nossos Clientes 
+import Tobias from '../../assets/Tobias.png'
+import Bruce from '../../assets/Bruce.png'
+import Perna_Longa from '../../assets/Perna-longa.png'
+import Lili from '../../assets/Lili.png'
+
+class Clientes extends Component {
+    render(){
+        return (   
+                <div id="clientes-container">
+                    <div id="titulo">
+                        Clientes Felizes
+                    </div>
+
+                    <div id="clientes-list-container">
+                        <div id="clientes-list">
+                            '<div id="cliente">
+                                <img src={Bruce} />
+                                <h3> Bruce</h3>
+                            </div>
+
+                            <div id="cliente">
+                                <img src={Tobias} />
+                                <h3> Tobias</h3>
+                            </div>
+
+                            <div id="cliente">
+                                <img src={Perna_Longa} />
+                                <h3> Perna-Longa</h3>
+                            </div>
+
+                            <div id="cliente">
+                                <img src={Lili} />
+                                <h3> Lili</h3>
+                            </div>
+'
+                        </div>
+                    </div>
+                </div>
+        );
+    }
+}
+
+export default Clientes;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,9 +1,15 @@
 import React from 'react'; 
+import Header from '../../components/Header'
+import Clientes from '../../components/Clientes'
+import './styles.css'
 
 function HomePage() {
     return(
         <div>
+            <Header />
             <h1>HomePage</h1>
+            <Clientes />
+
         </div>
     );
 }
